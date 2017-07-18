d9e31e40c87002c6e0785a1ddbb230208bd372b9
이현규 <@LAPTOP-98HF75ME>
2017-07-18 14:05:34 +0900

    PropertyManager 클래스 변경

--- Pofoland/src/test/java/db/DBConnectionTest.java
+++ Pofoland/src/test/java/db/DBConnectionTest.java
@@ -2,6 +2,8 @@ package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 import javax.inject.Inject;
@@ -28,30 +30,45 @@ public class DBConnectionTest {
 	
 	@Inject SqlSessionFactoryBean factoryBean;
 	
+	@Inject PropertyManager propertyManager;
+	
 	@Test
-	public void dbConnectionTest() {
-		
-		PropertyManager propManager = new PropertyManager("db.server.properties");
-		
-		Properties prop = propManager.getProperties();
-		
-		String driver = prop.getProperty("jdbc.driverClassName");
-		String url = prop.getProperty("jdbc.url");
-		String username = prop.getProperty("jdbc.username");
-		String password = prop.getProperty("jdbc.password");
-		
-		try {
-			Class.forName(driver);
-			
-			Connection conn =  DriverManager.getConnection(url, username, password);
-			
-			LoggerManager.info(getClass(), "커넥션 여부 : {}" , !conn.isClosed());
-		} catch (SQLException e) {
-			LoggerManager.info(getClass(), "커넥션 실패");
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		LoggerManager.info(getClass(), "커넥션성공");
+	public void propertiesTest() {
+      Properties properties = propertyManager.getProperties();
+      
+      System.out.println("현재 운영체제 : " + System.getProperty("os.name"));
+      
+      System.out.println("DB URL : " + properties.get("jdbc.url"));
+      System.out.println("개발 : " + properties.get("dev.image.path"));
+      System.out.println("운영 : " + properties.get("operate.image.path"));
+      
+      System.out.println("현재 시스템 환경에 따른 이미지 경로 : " + properties.get("image.path"));
 	}
+	
+//	@Test
+//	public void dbConnectionTest() {
+//		
+//		PropertyManager propManager = new PropertyManager("db.server.properties");
+//		
+//		Properties prop = propManager.getProperties();
+//		
+//		String driver = prop.getProperty("jdbc.driverClassName");
+//		String url = prop.getProperty("jdbc.url");
+//		String username = prop.getProperty("jdbc.username");
+//		String password = prop.getProperty("jdbc.password");
+//		
+//		try {
+//			Class.forName(driver);
+//			
+//			Connection conn =  DriverManager.getConnection(url, username, password);
+//			
+//			LoggerManager.info(getClass(), "커넥션 여부 : {}" , !conn.isClosed());
+//		} catch (SQLException e) {
+//			LoggerManager.info(getClass(), "커넥션 실패");
+//			e.printStackTrace();
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//		LoggerManager.info(getClass(), "커넥션성공");
+//	}
 }
