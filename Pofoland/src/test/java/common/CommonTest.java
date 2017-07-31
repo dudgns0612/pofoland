8bbe57f8d88b5c550f683aeb26760d9b70e82646
User-PC\User <dudgns0612@naver.com>
2017-07-31 17:21:49 +0900

    - 오류수정 -

--- Pofoland/src/test/java/common/CommonTest.java
+++ Pofoland/src/test/java/common/CommonTest.java
@@ -2,14 +2,10 @@ package common;
 
 import org.junit.Test;
 
-import com.hst.pofoland.common.auth.MailAuthentication;
-
 public class CommonTest {
 	
 	@Test
 	public void Main() {
-		MailAuthentication mailAuth = new MailAuthentication("dudgns0612@gmail.com", "");
-		
-		mailAuth.sendAuthMail();
+
 	}
 }
